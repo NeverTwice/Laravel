70ab72317b2038c7c3659de1672112e40ad9869d
NeverTwice <samuelantunes@hotmail.fr>
2017-03-10 14:13:16 +0000

    Table Seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+
+    $name = strtolower(str_random(10));
+    return [
+        'typeid' => 1,
+        'siteid' =>1,
+        'code' => $name,
+        'label' => $name,
+
+    ];
+});
